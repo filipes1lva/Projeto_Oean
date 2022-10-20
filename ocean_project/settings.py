2ccf056b15d117452a298ea56d0f188a53052244
Filipe Silva <filipesilvados@gmail.com>
2022-10-20 09:30:41 -0400

    Quarto commit: Atualização das telas de login e logout, além da lógica da aplicação - caminho logico do usuário

--- ocean_project/settings.py
+++ ocean_project/settings.py
@@ -129,6 +129,8 @@ STATTICFILES_DIRS = [
 
 STATIC_ROOT = (BASE_DIR/"asert/")
 
+LOGIN_REDIRECT_URL = 'dashboard-index'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
