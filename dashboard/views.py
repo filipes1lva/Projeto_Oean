d6632cac079f11d386266996c50aa8c6410e54d1
Filipe Silva <filipesilvados@gmail.com>
2022-10-20 11:17:20 -0400

    Quinto commit: Criação tela de perfil e inclusão da foto de perfil

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
 
 
 # Create your views here.
 
+
+@login_required
 def index(request):
     return render(request, 'dashboard/index.html')
     #return HttpResponse('<h1 style="color:blue;"> Esta é a página de index </h1>')
 
+@login_required
 def staff(request):
     return render(request, 'dashboard/staff.html')
 
+@login_required
 def products(request):
     return render(request, 'dashboard/products.html')
 
+@login_required
 def order(request):
     return render(request, 'dashboard/order.html')
