464a71f424a8993c146c65208c3e14f90ead1cf3
Filipe Silva <filipesilvados@gmail.com>
2022-10-19 20:44:54 -0400

    Segundo commit

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
-from .models import Products
+from .models import Products, Order
 from django.contrib.auth.models import Group
-
 
 
 admin.site.site_header = 'Autobots Control Dashboard'
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'quantidade', 'categoria')
+    list_filter = ['categoria']
+
+
 # Register your models here.
 
-admin.site.register(Products)
+admin.site.register(Products, ProductAdmin)
+admin.site.register(Order)
 # admin.site.unregister(Group)
+
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,6 +1,12 @@
+from pickle import TRUE
+from pyexpat import model
 from random import choices
 from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.contrib.auth.models import User
+
+
+from dashboard.views import staff
 
 # Create your models here.
 
@@ -16,5 +22,26 @@ class Products(models.Model):
     categoria = models.CharField(max_length=20, choices=CATEGORIA)
     quantidade = models.PositiveBigIntegerField(null=True)
 
+    
+    class Meta:
+        verbose_name_plural = 'Produtos'
+
+
+
     def __str__(self) -> str:
         return f'{self.nome}-{self.quantidade}'
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Products, on_delete=models.CASCADE, null=True)
+    staff = models.ForeignKey(User, models.CASCADE, null=True)
+    order_quantity = models.PositiveIntegerField(null=True)
+    date = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Pedidos'
+
+
+
+    def __str__(self):
+        return f'{self.product} ordered by {self.staff.username}'
